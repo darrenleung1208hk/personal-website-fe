211cfcfdc3cbc9e87f1380848964849dae703063
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-08-21 17:31:01 +0800

    added nav items

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,14 +1,35 @@
-import { AppBar as MuiAppBar, Box, Toolbar, Typography } from "@mui/material";
+import {
+	AppBar as MuiAppBar,
+	Box,
+	Button,
+	Stack,
+	Toolbar,
+	Typography,
+} from "@mui/material";
+import Link from "next/link";
 
-type Props = {};
+const navItems = [
+	{ name: "About", href: "#about" },
+	{ name: "Experience", href: "#experience" },
+	{ name: "Skills", href: "#skills" },
+	{ name: "Education", href: "#education" },
+];
 
-const AppBar = (props: Props) => {
+const AppBar = () => {
 	return (
 		<Box>
 			<Toolbar />
 			<MuiAppBar elevation={0}>
 				<Toolbar sx={{ display: "flex", justifyContent: "flex-end" }}>
-					<Typography>Experience</Typography>
+					<Stack direction="row" spacing={1}>
+						{navItems.map(({ name, href }) => (
+							<Link href={href} key={href}>
+								<Button variant="contained" disableElevation>
+									<Typography>{name}</Typography>
+								</Button>
+							</Link>
+						))}
+					</Stack>
 				</Toolbar>
 			</MuiAppBar>
 		</Box>
