0d61025986a6ba531a316829c0d65515a3e7b251
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-08-21 16:58:08 +0800

    removed bg color

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,4 +1,5 @@
 import { createTheme } from "@mui/material";
+import { lightBlue } from "@mui/material/colors";
 
 const theme = createTheme({
 	palette: {
@@ -6,21 +7,12 @@ const theme = createTheme({
 			main: "#1a237e",
 			light: "#534bae",
 			dark: "#000051",
-			contrastText: "#fafafa",
 		},
 		secondary: {
 			main: "#ffd54f",
 			light: "#ffff81",
 			dark: "#c8a415",
 			contrastText: "#455a64",
-		},
-		background: {
-			default: "#1a237e",
-			paper: "#534bae",
-		},
-		text: {
-			primary: "#fafafa",
-			secondary: "#455a64",
 		},
 	},
 	typography: {
