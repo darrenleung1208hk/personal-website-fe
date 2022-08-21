f508b12a1ae1ea6414896eca492f12e8542e1a50
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-08-21 19:18:54 +0800

    responsive hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,36 @@
-import { Container, Typography } from "@mui/material";
+import {
+	Box,
+	Container,
+	Typography,
+	useMediaQuery,
+	useTheme,
+} from "@mui/material";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+	const theme = useTheme();
+	const mdUp = useMediaQuery(theme.breakpoints.up("md"));
+
 	return (
-		<Container sx={{ py: "20vh" }}>
-			<Typography variant="h5">Hi there! I am...</Typography>
-			<Typography variant="h2" fontWeight={700} color="primary.main" paragraph>
-				Darren Leung.
-			</Typography>
-			<Typography variant="h6">
-				I'm a front-end web developer experienced in headless <br />
-				CMS integration. I implement responsive frontends using <br />
-				React and the Material UI library to build exceptional
-				<br />
-				user experiences.
-			</Typography>
+		<Container sx={{ py: { md: "10vh", lg: "20vh" } }}>
+			<Box width={{ lg: "60%" }}>
+				<Typography variant="h5" paragraph={!mdUp}>
+					Hi there! I am...
+				</Typography>
+				<Typography
+					variant={mdUp ? "h2" : "h3"}
+					fontWeight={700}
+					color="primary.main"
+					paragraph
+				>
+					Darren Leung.
+				</Typography>
+				<Typography variant="h6">
+					I'm a front-end web developer experienced in headless CMS integration.
+					I implement responsive frontends using React and the Material UI
+					library to build exceptional user experiences.
+				</Typography>
+			</Box>
 		</Container>
 	);
 };
