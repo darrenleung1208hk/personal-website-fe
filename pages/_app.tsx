e8c0ea3772225d2c1eecbfadffcb99b9e542f16e
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-08-21 19:45:15 +0800

    added drawer to root component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@fontsource/poppins";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
-import { AppBar } from "../components";
+import { AppBar, NavigationDrawer } from "../components";
 import "../styles/globals.css";
 import theme from "../styles/theme";
 
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
 			<AppBar />
+			<NavigationDrawer />
 			<Component {...pageProps} />
 		</ThemeProvider>
 	);
