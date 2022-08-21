a75594bff76b2dd0a0ec49185471304e44ea6f7e
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-08-21 18:28:34 +0800

    hero section basic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
+import { Container, Typography } from "@mui/material";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-	return <div>hello world</div>;
+	return (
+		<Container sx={{ py: "15vh" }}>
+			<Typography variant="h5">Hi there! I am...</Typography>
+			<Typography variant="h2" fontWeight={700} color="primary.main" paragraph>
+				Darren Leung.
+			</Typography>
+			<Typography>
+				I'm a front-end web developer experienced in headless <br />
+				CMS integration. I implement responsive frontends using <br />
+				React and the Material UI library to build exceptional
+				<br />
+				user experiences.
+			</Typography>
+		</Container>
+	);
 };
 
 export default Home;
