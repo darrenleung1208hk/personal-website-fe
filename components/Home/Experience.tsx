05c5a3864aee42efcafa1fc5e54bf6a3b0c0523d
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-08-22 19:57:42 +0800

    added spacing between each job

--- components/Home/Experience.tsx
+++ components/Home/Experience.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 
 const Experience = () => {
 	return (
@@ -12,45 +12,51 @@ const Experience = () => {
 				>
 					Work Experience.
 				</Typography>
-				<Typography variant="h5" fontWeight={700} paragraph>
-					Web Developer,{" "}
-					<a href="https://www.smartdrop.com.hk/" target="_blank">
-						Smart Drop
-					</a>
-				</Typography>
-				<Typography>Jun 2021 -Present</Typography>
-				<Typography component="div">
-					<ul>
-						<li>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-							varius sapien.
-						</li>
-						<li>Aenean accumsan sagittis semper.</li>
-						<li>Integer nec pretium massa, sit amet porta libero.</li>
-						<li>
-							Sed hendrerit consectetur suscipit. Maecenas sagittis bibendum
-							metus ac condimentum.
-						</li>
-					</ul>
-				</Typography>
-				<Typography variant="h5" fontWeight={700} paragraph>
-					Intern, Technology-Enriched Learning Initiative
-				</Typography>
-				<Typography>Jun 2020 - Aug 2020</Typography>
-				<Typography component="div">
-					<ul>
-						<li>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-							varius sapien.
-						</li>
-						<li>Aenean accumsan sagittis semper.</li>
-						<li>Integer nec pretium massa, sit amet porta libero.</li>
-						<li>
-							Sed hendrerit consectetur suscipit. Maecenas sagittis bibendum
-							metus ac condimentum.
-						</li>
-					</ul>
-				</Typography>
+				<Stack spacing={2}>
+					<Box>
+						<Typography variant="h5" fontWeight={700} paragraph>
+							Web Developer,{" "}
+							<a href="https://www.smartdrop.com.hk/" target="_blank">
+								Smart Drop
+							</a>
+						</Typography>
+						<Typography>Jun 2021 -Present</Typography>
+						<Typography component="div">
+							<ul>
+								<li>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+									non varius sapien.
+								</li>
+								<li>Aenean accumsan sagittis semper.</li>
+								<li>Integer nec pretium massa, sit amet porta libero.</li>
+								<li>
+									Sed hendrerit consectetur suscipit. Maecenas sagittis bibendum
+									metus ac condimentum.
+								</li>
+							</ul>
+						</Typography>
+					</Box>
+					<Box>
+						<Typography variant="h5" fontWeight={700} paragraph>
+							Intern, Technology-Enriched Learning Initiative
+						</Typography>
+						<Typography>Jun 2020 - Aug 2020</Typography>
+						<Typography component="div">
+							<ul>
+								<li>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+									non varius sapien.
+								</li>
+								<li>Aenean accumsan sagittis semper.</li>
+								<li>Integer nec pretium massa, sit amet porta libero.</li>
+								<li>
+									Sed hendrerit consectetur suscipit. Maecenas sagittis bibendum
+									metus ac condimentum.
+								</li>
+							</ul>
+						</Typography>
+					</Box>
+				</Stack>
 			</Box>
 		</Container>
 	);
