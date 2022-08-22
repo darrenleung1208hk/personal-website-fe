ea076049f1ef2a09f62720460c7440e1c7a0af15
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-08-22 15:45:46 +0800

    experience section basic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,6 +82,46 @@ const Home: NextPage = () => {
 					</Box>
 				</Container>
 			</ParallaxLayer>
+			<ParallaxLayer
+				id="experience"
+				offset={2}
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
+				<Container>
+					<Box width={{ lg: "60%" }}>
+						<Typography
+							variant="h3"
+							fontWeight={700}
+							color="primary.main"
+							paragraph
+						>
+							Work Experience.
+						</Typography>
+						<Typography variant="h5" fontWeight={700} paragraph>
+							Web Developer, Smart Drop
+						</Typography>
+						<Typography>Jun 2021 -Present</Typography>
+						<Typography component="div">
+							<ul>
+								<li>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+									non varius sapien.
+								</li>
+								<li>Aenean accumsan sagittis semper.</li>
+								<li>Integer nec pretium massa, sit amet porta libero.</li>
+								<li>
+									Sed hendrerit consectetur suscipit. Maecenas sagittis bibendum
+									metus ac condimentum.
+								</li>
+							</ul>
+						</Typography>
+					</Box>
+				</Container>
+			</ParallaxLayer>
 		</Box>
 	);
 };
