383685750536effa0ef1940f623c5af3d501127d
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-08-22 19:34:01 +0800

    render multiple job experience

--- components/Home/Experience.tsx
+++ components/Home/Experience.tsx
@@ -36,6 +36,24 @@ const Experience = () => {
 						</li>
 					</ul>
 				</Typography>
+				<Typography variant="h5" fontWeight={700} paragraph>
+					Intern, Technology-Enriched Learning Initiative
+				</Typography>
+				<Typography>Jun 2020 - Aug 2020</Typography>
+				<Typography component="div">
+					<ul>
+						<li>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
+							varius sapien.
+						</li>
+						<li>Aenean accumsan sagittis semper.</li>
+						<li>Integer nec pretium massa, sit amet porta libero.</li>
+						<li>
+							Sed hendrerit consectetur suscipit. Maecenas sagittis bibendum
+							metus ac condimentum.
+						</li>
+					</ul>
+				</Typography>
 			</Box>
 		</Container>
 	);
