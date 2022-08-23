4c0dfc98f200b934558b3a131d93af5e8eedb4de
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-08-23 13:08:19 +0800

    created api to fetch work experience

---
+++ contentful/api.ts
@@ -0,0 +1,32 @@
+async function fetchGraphQL(query: string) {
+	return fetch(
+		`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+			},
+			body: JSON.stringify({ query }),
+		}
+	).then((response) => response.json());
+}
+
+export async function getAllWorkExperience() {
+	const entries = await fetchGraphQL(
+		`query {
+      workExperienceCollection {
+        items {
+          jobTitle
+          companyName
+          companyWebsite
+          jobLocation
+          startDate
+          endDate
+          jobDuties
+        }
+      }
+    }`
+	);
+	return entries?.workExperienceCollection?.items;
+}
