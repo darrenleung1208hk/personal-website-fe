87383317e1ce45a37ac63954fea15cf3b92f9c7e
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-04 16:33:11 +0800

    hero section responsive layout

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,4 +1,5 @@
 import {
+	Box,
 	Grid,
 	Stack,
 	Typography,
@@ -11,18 +12,23 @@ import SectionWrapper from "../SectionWrapper";
 
 interface Props extends IHero {}
 
+const heroImageBorderRadius = 2;
+
 const Hero = ({ greeting, name, headline, descriptions }: Props) => {
-	const { breakpoints } = useTheme();
+	const { breakpoints, spacing } = useTheme();
 	const mdUp = useMediaQuery(breakpoints.up("md"));
 
 	return (
 		<SectionWrapper>
 			<Stack minHeight="50vh" justifyContent="center">
-				<Grid container alignItems="center">
-					<Grid item md={6}>
-						<Typography variant="h5" paragraph={!mdUp}>
-							{greeting}
-						</Typography>
+				<Grid
+					container
+					direction={{ xs: "column-reverse", sm: "row" }}
+					alignItems="center"
+					justifyContent="space-between"
+				>
+					<Grid item xs={12} sm={6} md={7}>
+						<Typography variant="h5">{greeting}</Typography>
 						<Typography
 							variant={mdUp ? "h2" : "h3"}
 							fontWeight={700}
@@ -45,8 +51,23 @@ const Hero = ({ greeting, name, headline, descriptions }: Props) => {
 							</Typography>
 						))}
 					</Grid>
-					<Grid item md={6}>
-						<Image src="/hero.jpg" width={1600} height={1200} />
+					<Grid item xs={12} sm md>
+						<Box
+							sx={{
+								width: { xs: `calc(100vw - 32px)`, sm: "100%" },
+								borderRadius: spacing(heroImageBorderRadius),
+								boxShadow: 12,
+							}}
+						>
+							<Image
+								src="/hero.jpg"
+								alt="hero-image"
+								width={1600}
+								height={1200}
+								layout="responsive"
+								style={{ borderRadius: spacing(heroImageBorderRadius) }}
+							/>
+						</Box>
 					</Grid>
 				</Grid>
 			</Stack>
