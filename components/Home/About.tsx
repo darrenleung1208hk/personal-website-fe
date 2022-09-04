6330fa907897844b3881fd8e6b9b503a94c4eaff
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-04 21:39:18 +0800

    removed container

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -7,30 +7,28 @@ interface Props extends IAbout {}
 const About = ({ description, skills }: Props) => {
 	return (
 		<SectionWrapper id="about">
-			<Container maxWidth="md">
-				<Typography
-					variant="h3"
-					fontWeight={700}
-					color="primary.main"
-					align="center"
-					paragraph
-				>
-					About Me.
-				</Typography>
-				<Typography variant="subtitle1" align="center" paragraph>
-					{description}
-				</Typography>
-				<Typography variant="subtitle1" align="center" paragraph>
-					Here's my tech stack:
-				</Typography>
-				<Grid container justifyContent="center" spacing={1}>
-					{skills?.map((skill, index) => (
-						<Grid key={`skill-${index}`} item>
-							<Chip label={skill} color="primary" />
-						</Grid>
-					))}
-				</Grid>
-			</Container>
+			<Typography
+				variant="h3"
+				fontWeight={700}
+				color="primary.main"
+				align="center"
+				paragraph
+			>
+				About Me.
+			</Typography>
+			<Typography variant="subtitle1" align="center" paragraph>
+				{description}
+			</Typography>
+			<Typography variant="subtitle1" align="center" paragraph>
+				Here's my tech stack:
+			</Typography>
+			<Grid container justifyContent="center" spacing={1}>
+				{skills?.map((skill, index) => (
+					<Grid key={`skill-${index}`} item>
+						<Chip label={skill} color="primary" />
+					</Grid>
+				))}
+			</Grid>
 		</SectionWrapper>
 	);
 };
