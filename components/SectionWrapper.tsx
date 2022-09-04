fe386e43a51b07131a5289e91b26c70f2c42eeb4
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-04 14:11:43 +0800

    removed min height and max width

--- components/SectionWrapper.tsx
+++ components/SectionWrapper.tsx
@@ -19,18 +19,13 @@ const SectionWrapper = ({
 	return (
 		<Box
 			id={id}
-			minHeight={{ xs: "50vh", lg: "100vh" }}
 			display="flex"
 			flexDirection="column"
 			justifyContent="center"
 			bgcolor={bgcolor || (dark && "primary.main") || "white"}
 		>
 			<Container>
-				<Box
-					py={5}
-					width={{ lg: "75%" }}
-					color={color || dark ? "white" : "black"}
-				>
+				<Box py={5} color={color || dark ? "white" : "black"}>
 					{children}
 				</Box>
 			</Container>
