ef61ee8401b499096fec5015cc3e33345c988e31
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-04 22:03:12 +0800

    dark background and inner shadow

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -1,35 +1,43 @@
-import { Chip, Container, Grid, Typography } from "@mui/material";
+import { Box, Chip, Container, Grid, Typography } from "@mui/material";
 import { IAbout } from "../../contentful";
-import SectionWrapper from "../SectionWrapper";
 
 interface Props extends IAbout {}
 
 const About = ({ description, skills }: Props) => {
 	return (
-		<SectionWrapper id="about">
-			<Typography
-				variant="h3"
-				fontWeight={700}
-				color="primary.main"
-				align="center"
-				paragraph
-			>
-				About Me.
-			</Typography>
-			<Typography variant="subtitle1" align="center" paragraph>
-				{description}
-			</Typography>
-			<Typography variant="subtitle1" align="center" paragraph>
-				Here's my tech stack:
-			</Typography>
-			<Grid container justifyContent="center" spacing={1}>
-				{skills?.map((skill, index) => (
-					<Grid key={`skill-${index}`} item>
-						<Chip label={skill} color="primary" />
-					</Grid>
-				))}
-			</Grid>
-		</SectionWrapper>
+		<Box
+			id="about"
+			py={8}
+			bgcolor="primary.main"
+			sx={{
+				boxShadow: `inset 0px 8px 4px -6px rgba(0,0,0,0.14), inset 0px -8px 4px -6px rgba(0,0,0,0.14)`,
+			}}
+		>
+			<Container>
+				<Typography
+					variant="h3"
+					fontWeight={700}
+					color="#fff"
+					align="center"
+					paragraph
+				>
+					About Me.
+				</Typography>
+				<Typography variant="subtitle1" align="center" color="#fff" paragraph>
+					{description}
+				</Typography>
+				<Typography variant="subtitle1" align="center" color="#fff" paragraph>
+					Here's my tech stack:
+				</Typography>
+				<Grid container justifyContent="center" spacing={1}>
+					{skills?.map((skill, index) => (
+						<Grid key={`skill-${index}`} item>
+							<Chip label={skill} color="secondary" sx={{ boxShadow: 4 }} />
+						</Grid>
+					))}
+				</Grid>
+			</Container>
+		</Box>
 	);
 };
 
