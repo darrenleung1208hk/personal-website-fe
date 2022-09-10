91d1c4054281097e1d1f68134b8790a92e49d09c
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-10 18:22:45 +0800

    removed container, replaced texts with carousel component

--- components/Home/Experience/Experience.tsx
+++ components/Home/Experience/Experience.tsx
@@ -1,6 +1,6 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
-import format from "date-fns/format";
+import { Box, Typography } from "@mui/material";
 import { IJobExperience } from "../../../contentful";
+import Timeline from "./Timeline";
 
 type Props = {
 	data: IJobExperience[];
@@ -11,42 +11,16 @@ const formatPattern = "MMM yyyy";
 const Experience = ({ data }: Props) => {
 	return (
 		<Box id="experience" py={12}>
-			<Container>
-				<Typography
-					variant="h3"
-					fontWeight={700}
-					color="primary.main"
-					textAlign="center"
-					paragraph
-				>
-					Work Experience.
-				</Typography>
-				<Stack spacing={2}>
-					{data?.map((job, index) => (
-						<Box key={`job-${index}`}>
-							<Typography variant="h5" fontWeight={700} paragraph>
-								{job.jobTitle},{" "}
-								<a href={job.companyWebsite} target="_blank" rel="noreferrer">
-									{job.companyName}
-								</a>
-							</Typography>
-							<Typography>
-								{format(new Date(job.startDate), formatPattern)} -{" "}
-								{job.endDate
-									? format(new Date(job.endDate), formatPattern)
-									: "Present"}
-							</Typography>
-							<Typography component="div">
-								<Stack component="ul" spacing={2}>
-									{job.jobDescriptions?.items?.map(({ payload }, index) => (
-										<li key={`job-${index}-description-${index}`}>{payload}</li>
-									))}
-								</Stack>
-							</Typography>
-						</Box>
-					))}
-				</Stack>
-			</Container>
+			<Typography
+				variant="h3"
+				fontWeight={700}
+				color="primary.main"
+				textAlign="center"
+				paragraph
+			>
+				Work Experience.
+			</Typography>
+			<Timeline data={data} />
 		</Box>
 	);
 };
