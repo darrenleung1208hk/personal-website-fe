2277b1c90971d46275b0562ec123dd7aa3d20c7e
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-12 16:02:06 +0800

    implemented contact section according to new design

--- components/Home/Contact.tsx
+++ components/Home/Contact.tsx
@@ -1,31 +1,48 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Chip, Container, Grid, Typography } from "@mui/material";
 import { IContact } from "../../contentful";
-import SectionWrapper from "../SectionWrapper";
+import EmailIcon from "@mui/icons-material/Email";
 
 interface Props extends IContact {}
 
 const Contact = ({ description }: Props) => {
 	return (
-		<SectionWrapper dark id="contact">
-			<Typography variant="h3" fontWeight={700} color="white" paragraph>
-				Get in Touch.
-			</Typography>
-			<Typography color="white" paragraph>
-				{description}
-			</Typography>
-			<Box pt={2}>
-				<Button
-					disableElevation
-					variant="contained"
-					color="inherit"
-					sx={{ color: "primary.main" }}
-					component="a"
-					href="mailto:leungyatlong1208@gmail.com"
+		<Box
+			id="contact"
+			py={{ xs: 4, md: 12 }}
+			bgcolor="primary.main"
+			sx={{
+				boxShadow: `inset 0px 12px 6px -6px rgba(0,0,0,0.14), inset 0px -12px 6px -6px rgba(0,0,0,0.20)`,
+			}}
+		>
+			<Container>
+				<Typography
+					variant="h3"
+					fontWeight={700}
+					color="#fff"
+					textAlign="center"
+					paragraph
 				>
-					Let&apos;s Talk
-				</Button>
-			</Box>
-		</SectionWrapper>
+					Get in Touch.
+				</Typography>
+				<Typography variant="subtitle1" align="center" color="#fff" paragraph>
+					{description}
+				</Typography>
+				<Box pt={2} textAlign="center">
+					<Button
+						disableElevation
+						variant="contained"
+						color="secondary"
+						size="large"
+						startIcon={<EmailIcon />}
+						sx={{ borderRadius: "32px" }}
+						component="a"
+						href="mailto:leungyatlong1208@gmail.com"
+					>
+						Let&apos;s Talk
+					</Button>
+				</Box>
+			</Container>
+		</Box>
 	);
 };
 
