ab50f24b20dc360cea649e89b589c3db0cb09575
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-12 16:51:40 +0800

    responsive footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { Box, Container, Grid, IconButton, Typography } from "@mui/material";
 
 const Footer = () => {
 	return (
-		<Box bgcolor="primary.dark" color="#fff" pt={6} pb={3}>
+		<Box bgcolor="primary.dark" color="#fff" py={{ xs: 6, md: 2 }}>
 			<Container>
 				<Grid
 					container
@@ -13,12 +13,24 @@ const Footer = () => {
 					alignItems="center"
 					spacing={2}
 				>
-					<Grid item xs={4} container>
+					<Grid
+						item
+						xs={12}
+						md={6}
+						container
+						justifyContent={{ xs: "center", md: "flex-start" }}
+					>
 						<Typography variant="body2">
 							© {new Date().getFullYear()} Darren Leung. All Rights Reserved.
 						</Typography>
 					</Grid>
-					<Grid item xs container justifyContent="flex-end">
+					<Grid
+						item
+						xs
+						md
+						container
+						justifyContent={{ xs: "center", md: "flex-end" }}
+					>
 						<IconButton>
 							<LinkedInIcon sx={{ color: "#fff", fontSize: "36px" }} />
 						</IconButton>
