49d561321709586e2eeef0b3435aa5669e5c1f6f
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-19 00:10:23 +0800

    hide back to top button when on top

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -1,12 +1,30 @@
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import { Fab } from "@mui/material";
+import { useEffect, useState } from "react";
 
 const BackToTop = () => {
+	const [display, setDisplay] = useState("none");
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 0) {
+				setDisplay("inline-flex");
+			}
+			if (window.scrollY === 0) {
+				setDisplay("none");
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
 	return (
 		<Fab
 			color="primary"
 			size="small"
 			sx={{
+				display,
 				position: "fixed",
 				bottom: ({ spacing }) => spacing(2),
 				right: ({ spacing }) => spacing(2),
