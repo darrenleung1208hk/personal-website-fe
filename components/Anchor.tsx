d080f60e843566fce578bab07688140e87486b76
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-19 23:15:02 +0800

    created anchor component

---
+++ components/Anchor.tsx
@@ -0,0 +1,25 @@
+import { Box, Theme } from "@mui/material";
+import { CSSProperties } from "@mui/material/styles/createMixins";
+import React from "react";
+
+type Props = {
+	id: string;
+};
+
+const Anchor: React.FC<Props> = ({ id }) => {
+	return (
+		<Box
+			id={id}
+			position="relative"
+			top={({ breakpoints, mixins }) => ({
+				xs: `${-(mixins.toolbar.minHeight as number) || 0}px`,
+				lg: `${
+					-((mixins.toolbar[breakpoints.up("sm")] as CSSProperties)
+						.minHeight as number) || 0
+				}px`,
+			})}
+		/>
+	);
+};
+
+export default Anchor;
