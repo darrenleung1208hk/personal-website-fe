10b5baaedb43b586e088a2391e4c6e1f170325e0
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-19 16:14:45 +0800

    updated type scale and wrapped theme with responsive font sizes

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material";
 
 const theme = createTheme({
 	palette: {
@@ -16,12 +16,18 @@ const theme = createTheme({
 	},
 	typography: {
 		fontFamily: ["Ubuntu", "Inter"].join(","),
-		subtitle1: { fontFamily: "Inter" },
-		subtitle2: { fontFamily: "Inter" },
+		h1: { fontSize: "3.815rem" },
+		h2: { fontSize: "3.052rem" },
+		h3: { fontSize: "2.441rem" },
+		h4: { fontSize: "1.953rem" },
+		h5: { fontSize: "1.563rem" },
+		h6: { fontSize: "1.25rem" },
 		body1: { fontFamily: "Inter" },
-		body2: { fontFamily: "Inter" },
-		button: { textTransform: "none" },
+		body2: { fontFamily: "Inter", fontSize: "0.8rem" },
+		button: { textTransform: "none", fontSize: "0.8rem" },
+		caption: { fontSize: "0.64rem" },
+		overline: { fontSize: "0.64rem" },
 	},
 });
 
-export default theme;
+export default responsiveFontSizes(theme);
